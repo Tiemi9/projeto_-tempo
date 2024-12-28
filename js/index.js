5472454ef099668587356d17f802697be79a2f47
Cris Morita <cristina_tiemi.social@yahoo.com>
2024-12-28 19:38:19 -0300

    Add event listener input

--- js/index.js
+++ js/index.js
@@ -4,3 +4,9 @@ const input = document.querySelector('#location-form');
 form === null || form === void 0 ? void 0 : form.addEventListener('submit', (event) => {
     event.preventDefault();
 });
+input === null || input === void 0 ? void 0 : input.addEventListener('focus', (event) => {
+    input.setAttribute('placeholder', '');
+});
+input === null || input === void 0 ? void 0 : input.addEventListener('blur', (event) => {
+    input.setAttribute('placeholder', 'Insira sua localização...');
+});
--- ts/index.ts
+++ ts/index.ts
@@ -4,7 +4,14 @@ const input: HTMLInputElement | null = document.querySelector('#location-form')
 form?.addEventListener('submit', (event) => {
     event.preventDefault()
 
-
+    
 })
 
-
+input?.addEventListener('focus', (event) => {
+    input.setAttribute('placeholder', '')  
+    
+})
+input?.addEventListener('blur', (event) => {
+    input.setAttribute('placeholder', 'Insira sua localização...')  
+    
+})
